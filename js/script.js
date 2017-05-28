36c523fa80f9e1e57a38f2ee99f18e7366cd0ef3
Sofie Den Haeze <Sofie@MacBook-Pro-van-Sofie.local>
2017-05-28 11:57:05 +0200

    WIP

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // JavaScript Document
 
 function myMap() {
-var myLatlng = new google.maps.LatLng(51.037855, 3.709588); 
+var myLatlng = new google.maps.LatLng(51.040955, 3.719832);
 var mapOptions = {
   zoom: 15,
   center: myLatlng
@@ -21,25 +21,24 @@ marker.setMap(map);
 
 (function(){
   
-var counter = 0, // to keep track of current slide
-    $items = document.querySelectorAll('.diy-slideshow figure'), // a collection of all of the slides, caching for performance
-    numItems = $items.length; // total number of slides
+var counter = 0,
+    $items = document.querySelectorAll('.diy-slideshow figure'),
+    numItems = $items.length; 
 
-// this function is what cycles the slides, showing the next or previous slide and hiding all the others
+
 var showCurrent = function(){
-  var itemToShow = Math.abs(counter%numItems);// uses remainder (aka modulo) operator to get the actual index of the element to show  
+  var itemToShow = Math.abs(counter%numItems);  
   
-  // remove .show from whichever element currently has it 
-  // http://stackoverflow.com/a/16053538/2006057
+
   [].forEach.call( $items, function(el){
     el.classList.remove('show');
   });
   
-  // add .show to the one item that's supposed to have it
+
   $items[itemToShow].classList.add('show');    
 };
 
-// add click events to prev & next buttons 
+
 document.querySelector('.next').addEventListener('click', function() {
      counter++;
      showCurrent();
